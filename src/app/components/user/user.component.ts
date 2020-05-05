6d78fe2cdf19fa85355cb4ee305827ba59299e4b
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-05-05 12:29:37 +0100

    component created

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,9 +2,10 @@ import { Component }  from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  template: '<h2>John Doe</h2>'
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.html']
 })
 
 export class UserComponent {
-  
+
 }
