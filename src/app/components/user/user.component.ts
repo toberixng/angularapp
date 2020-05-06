d0f8ad78a5610c9222fec538010666dc08f13291
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-05-06 10:35:54 +0100

    array created

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit }  from '@angular/core';
 import { User } from '../../models/User';
 
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User'
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[]
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.users = [
+      {
+        firstName: 'Oluwatobi',
+        lastName:  'Fatumo',
+        age: 30,
+        address: {
+          street: '50 main st',
+          city: 'Boston',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Chioma',
+        lastName:  'Umeibe',
+        age: 32,
+        address: {
+          street: '50 Canal Estate st',
+          city: 'Cele Ago',
+          state: 'AN'
+        }
+      },
+      {
+        firstName: 'Abiola',
+        lastName:  'Sanni',
+        age: 45,
+        address: {
+          street: '20 Governor Road Ikotun',
+          city: 'Ikotun',
+          state: 'OY'
+        }
+      }
+    ]
   }
 
 }
