d37a3cdf549ed68fc659aafbb8fece1aa8c20ab6
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-05-06 16:49:32 +0100

    user component update

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,43 +9,47 @@ import { User } from '../../models/User'
 export class UsersComponent implements OnInit {
   users: User[]
   showExtended: boolean = true
-  loaded: boolean = true
+  loaded: boolean = false
 
   constructor() { }
 
   ngOnInit(){
-    this.users = [
-      {
-        firstName: 'Oluwatobi',
-        lastName:  'Fatumo',
-        age: 30,
-        address: {
-          street: '50 main st',
-          city: 'Boston',
-          state: 'MA'
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'Oluwatobi',
+          lastName:  'Fatumo',
+          age: 30,
+          address: {
+            street: '50 main st',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Chioma',
+          lastName:  'Umeibe',
+          age: 32,
+          address: {
+            street: '50 Canal Estate st',
+            city: 'Cele Ago',
+            state: 'AN'
+          }
+        },
+        {
+          firstName: 'Abiola',
+          lastName:  'Sanni',
+          age: 45,
+          address: {
+            street: '20 Governor Road Ikotun',
+            city: 'Ikotun',
+            state: 'OY'
+          }
         }
-      },
-      {
-        firstName: 'Chioma',
-        lastName:  'Umeibe',
-        age: 32,
-        address: {
-          street: '50 Canal Estate st',
-          city: 'Cele Ago',
-          state: 'AN'
-        }
-      },
-      {
-        firstName: 'Abiola',
-        lastName:  'Sanni',
-        age: 45,
-        address: {
-          street: '20 Governor Road Ikotun',
-          city: 'Ikotun',
-          state: 'OY'
-        }
-      }
-    ]
+      ]
+      this.loaded = true;
+    }, 2000)
+    
 
     this.showExtended = false;
 
